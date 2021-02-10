702e351867c2c8a6ae366a9ccd310365bf76e3de
Sahil Kargutkar <sahilkargutkar.sk@gmail.com>
2021-02-10 17:28:14 +0530

    ain

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+import React,{useState} from 'react'
+import './App.css';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'
+import Welcome from './pages/Welcome'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+
+export const CredentialsContext = React.createContext();
+
+function App() {
+  const credentialsState = useState(null);
+  
+  return (
+    <div className="App">
+      <CredentialsContext.Provider value={credentialsState}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+
+      </CredentialsContext.Provider>
+    </div>
+  );
+}
+
+export default App;
